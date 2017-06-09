dbf971c71fe612e7005f59d102feed54899a9d9e
Dominik Menke <dom@digineo.de>
2017-06-09 18:27:11 +0200

    provide a conveniance function for cross-compilation
    See _examples/Makefile for a usage sample.

--- goldflags.mk
+++ goldflags.mk
@@ -5,8 +5,22 @@ COMMIT_UNIX ?= $(shell git show -s --format=%ct HEAD)
 BUILD_COUNT ?= $(shell git rev-list --count HEAD)
 BUILD_UNIX  ?= $(shell date +%s)
 
-GOLDFLAGS = \
+GOLDFLAGS = -s -w \
 	-X 'github.com/digineo/goldflags.commitID=$(COMMIT_ID)' \
 	-X 'github.com/digineo/goldflags.commitUnix=$(COMMIT_UNIX)' \
 	-X 'github.com/digineo/goldflags.buildCount=$(BUILD_COUNT)' \
 	-X 'github.com/digineo/goldflags.buildUnix=$(BUILD_UNIX)'
+
+# Build a platform specific version of $target. It creates a
+# "$(basename $target)" (or "$(basename).exe" for Windows) in the
+# directory "$(dirname $target)/$(goos)-$(goarch)", e.g.
+# "bin/linux-amd64/main" for target=bin/main.
+#
+# call-seq:
+#	$(call goldbuild,target,goos,goarch)
+define goldbuild
+	@mkdir -p "$(dir $(1))/$(2)-$(3)"
+	GOOS="$(2)" GOARCH="$(3)" go build \
+		-ldflags "$(GOLDFLAGS)" \
+		-o "$(dir $(1))/$(2)-$(3)/$(notdir $(1))$(if $(filter windows,$(2)),.exe)"
+endef
